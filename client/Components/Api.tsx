60f808e09b8fcae35f46b6cadda4eeb21d94ef32
Muhammed Said Arı <sait1223ari@gmail.com>
2025-12-06 13:22:19 +0300

    db.json değişti

--- client/Components/Api.tsx
+++ client/Components/Api.tsx
@@ -2,9 +2,7 @@ import { Platform } from "react-native";
 
 // API URL'ini belirle (Android emulator vs iOS simulator)
 const URL =
-  Platform.OS === "android"
-    ? "http://10.0.2.2:3001/" // Android için özel IP
-    : "http://localhost:3001/"; // GitHub repo: ProjeYonetici
+  "https://my-json-server.typicode.com/MSaidari/ProjeYonetici/"; // GitHub repo: ProjeYonetici
 
 // "https://my-json-server.typicode.com/MSaidari/ProjeYonetici/"
 // "android"
